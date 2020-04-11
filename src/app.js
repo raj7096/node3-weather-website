dc754e973189aae3586a4566f02a96f1b4f7a943
Raj Rathore <rathoreraj725@gmail.com>
2020-04-11 16:36:32 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utlis/geocode')
 const forecast = require('./utlis/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define Path For Express Config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -84,6 +85,6 @@ app.get('*',(req, res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server Is On At 3000')
+app.listen(port,()=>{
+    console.log('Server Is On At' +port)
 })
